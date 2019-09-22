330020ed7d9bdf3e619f9612a6d2a92a9406c872
Iliger <imistrafik@yandex.ru>
2019-09-22 16:13:19 +0300

    Added router to app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import MainContainer from './containers/MainContainer'
-import AdditionalContainer from './containers/AdditionalContainer'
+import Switch from "react-router-dom/es/Switch";
+import Route from "react-router-dom/es/Route";
 
-function App() {
-  return (
-    <div>
-      <MainContainer />
-      <AdditionalContainer />
-    </div>
-  );
+import MainContainer from './containers/MainContainer'
+import AdditionalContainer from "./containers/AdditionalContainer";
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Switch>
+                    <Route exact path='/' component={MainContainer}/>
+                    <Route path='/additional' component={AdditionalContainer}/>
+                </Switch>
+            </div>
+        )
+    }
 }
 
 export default App;
--- src/containers/AdditionalContainer.jsx
+++ src/containers/AdditionalContainer.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 class AdditionalContainer extends React.Component {
     render () {
-        return <div>123</div>
+        return (
+        <div>
+            <h1>Второстепенная</h1>
+            <div>
+                <ul>
+                    <li>
+                        <Link to={`/`}>Главная</Link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        )
     }
 }
 
