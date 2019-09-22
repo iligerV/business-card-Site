43857134d9c235cd3aa9288b5debf32c699337a2
Iliger <imistrafik@yandex.ru>
2019-09-22 15:17:10 +0300

    Initialized app

---
+++ src/containers/AdditionalContainer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class AdditionalContainer extends React.Component {
+    render () {
+        return <div>123</div>
+    }
+}
+
+export default AdditionalContainer;
---
+++ src/containers/MainContainer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class MainContainer extends React.Component {
+    render () {
+        return <div>Один два Три</div>
+    }
+}
+
+export default MainContainer;
